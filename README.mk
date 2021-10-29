d539afffa42d4fdded81ba962ff93e920dffab2c
Cassiano Augusto da Silva <cassianoas@hotmail.com>
2021-10-29 17:45:02 -0300

    Primeiro projeto FIAP

---
+++ README.mk
@@ -0,0 +1,24 @@
+## ENTERPRISE CONNECTION - IFOOD 🍔🍕🍟
+
+
+
+##### 📚 *Resumo / Summary*
+
+PT/BR - Desafio dado no segundo modulo da FIAP, usando conceitos básicos de Python (if, else, for, while...) e funções de lista simples o desafio era usar apenas o básico para filtrar e classificar seis restaurantes diferentes por classificação de nota e depois por distância, para classificar em casos onde as notas de classificação são iguais.
+
+EN - Challenge given in FIAP's second module, using basic Python concepts (if, else, for, while...) and simple list functions the challenge was to use just the basics to filter and sort six different restaurants by rating and then by distance, to classify in cases where the classification scores are equal.
+
+##### 👨‍🔧 *Mão na massa / get the hands dirty*
+
+PT/BR - Usando uma lista com Arrays, fizemos inicialmente um sort para classificar os restaurantes pela chave review que foi adicionada a uma função (já que não podíamos usar lambda), após isso fizemos a inclusão de uma lista vazia para os loops a seguir, uma vez que a função sort não armazena os valores e realizá-la uma segunda vez bagunçaria o primeiro filtro.
+Usando as condicionais if e else em loops de while fizemos a segunda classificação dos restaurantes pela distância que era o segundo critério caso as avaliações fossem iguais.
+
+EN - Using a list with Arrays, we initially did a sort to sort the restaurants by the review key that was added to a function (since we couldn't use lambda), after that we added an empty list for the following loops, since the sort function does not store the values and performing it a second time would mess up the first filter.
+Using if and else conditionals in while loops we did the second ranking of restaurants by distance which was the second criterion if the ratings were equal.
+
+##### 🙌 *Conclusão / get the hands dirty*
+
+PT/BR - Um projeto modesto com ferramentas básicas feito de iniciantes para iniciantes!
+
+EN - A modest project with basic tools made by beginners for beginners!
+
